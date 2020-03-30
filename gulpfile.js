0614f6e3b281057d2630637c856f446b340fbba0
eiiizong <eiiizong@163.com>
2020-03-30 15:45:10 +0800

    css自动前缀

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,42 @@
-const { src, dest, parallel, task, series, watch } = require('gulp'),
-	connect = require('gulp-connect'),
-	del = require('del'),
-	sourcemaps = require('gulp-sourcemaps'),
-	cssnano = require('gulp-cssnano'),
-	gulpLess = require('gulp-less'),
-	LessAutoprefix = require('less-plugin-autoprefix'),
-	rename = require('gulp-rename'),
-	gulpSass = require('gulp-sass'),
-	autoprefix = new LessAutoprefix({
-		browsers: ['last 2 versions']
-	});
+const { src, dest, parallel, series, watch } = require('gulp'),
+  connect = require('gulp-connect'),
+  del = require('del'),
+  sourcemaps = require('gulp-sourcemaps'),
+  cssnano = require('gulp-cssnano'),
+  rename = require('gulp-rename'),
+  autoprefixer = require('gulp-autoprefixer'),
+  gulpSass = require('gulp-sass');
 
 gulpSass.compiler = require('node-sass');
 const delhtml = () => del(['dist/*.html']);
 const delcss = () => del(['dist/css/']);
+
 const delall = () => del(['dist']);
+const delCss = () => del(['dist/css']);
+// const delall = () => del(['dist']);
 
 const server = () => {
-	return connect.server({
-		root: 'dist',
-		livereload: true,
-		port: 8002
-	});
+  return connect.server({
+    root: 'dist',
+    livereload: true,
+    port: 8002
+  });
 };
 
-const less = () => {
-	return src('./src/styles/scss/style.scss')
-		.pipe(sourcemaps.init())
-		.pipe(
-			gulpSass({
-				plugins: [autoprefix]
-			}).on('error', gulpSass.logError)
-		)
-		.pipe(cssnano())
-		.pipe(rename('style.min.css'))
-		.pipe(sourcemaps.write('./'))
-		.pipe(dest('./dist/css'));
+const scss = () => {
+  return src('./src/styles/scss/index.scss')
+    .pipe(sourcemaps.init())
+    .pipe(gulpSass().on('error', gulpSass.logError))
+    .pipe(
+      autoprefixer({
+        // 是否美化属性 默认为 true
+        cascade: false
+      })
+    )
+    .pipe(cssnano())
+    .pipe(rename('index.min.css'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest('./dist/css'));
 };
 
 const reload = () => src('./dist/*.html').pipe(connect.reload());
@@ -49,12 +50,12 @@ const copyfont = () => src('./src/fonts/*.ttf').pipe(dest('./dist/fonts/'));
 const copy = parallel(copyimg, copyhtml, copycss, copyjs, copyfont, copylib);
 // 检测文件
 watch(['./src/*.html'], series(copyhtml, reload));
-watch(['./src/styles/css/*.css'], series(copycss, reload));
+watch(['./src/styles/css/*.css'], series(delCss,copycss, reload));
 watch(['./src/lib/**/*'], series(copylib, reload));
-watch(['./src/styles/scss/**/*'], series(less, reload));
+watch(['./src/styles/scss/**/*'], series(delCss,scss, reload));
 watch(['./src/js/*.js'], series(copyjs, reload));
 watch(['./src/images/**/*'], series(copyimg, reload));
 
-exports.less = less;
+exports.scss = scss;
 exports.delall = delall;
-exports.dev = series(copy, less, server);
+exports.dev = series(delall,copy, scss, server);
