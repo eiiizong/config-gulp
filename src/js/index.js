3be0b16f28b34fc336ebe26a457c696a0e3c19ad
eiiizong <eiiizong@163.com>
2020-05-20 17:21:50 +0800

    ...

--- src/js/index.js
+++ src/js/index.js
@@ -12,8 +12,6 @@ $(function () {
     }
   };
 
-  
-
   const runFunc = (componentId, funcName, ...rest) => {
     try {
       var result = window[componentId + '_' + funcName](...rest);
@@ -24,12 +22,11 @@ $(function () {
   const ajaxRequest = (url, data, callback) => {
     $.getJSON(url, data, function (json) {
       if (json.status === 200) {
-        if(callback && typeof callback === 'function') {
-          callback(json)
+        if (callback && typeof callback === 'function') {
+          callback(json);
         }
       } else {
-        callback(json)
-        console.error(json)
+        console.error(json);
       }
     });
   };
@@ -47,9 +44,11 @@ $(function () {
     }
 
     requestData.components.map(item => {
-      const ok = runFunc(item.id, 'check');
-      if (!ok) {
-        isOk = false;
+      if (item.form) {
+        const ok = runFunc(item.id, 'check');
+        if (!ok) {
+          isOk = false;
+        }
       }
     });
     return isOk;
@@ -59,77 +58,91 @@ $(function () {
     $('#' + componentId + '_wrapper').css(style);
   };
 
-  const submit = url => {
-    var isOk = check(); // 验证数据是否为空
-    let resultData = [];
-    if (!isOk) {
-      return;
-    }
-    requestData.components.map(item => {
-      resultData[item.id] = runFunc(item.id, 'getData');
-    });
-    $.post({
-      url: url,
-      // 此处需要告知传递参数类型为JSON，不能缺少
-      contentType: 'application/json',
-      // 将JSON转化为字符串传递
-      data: JSON.stringify(resultData),
-      // 成功后的方法
-      success: function (result) {
-        console.log('提交数据成功', result);
-      },
-    });
-  };
   const getData = () => {
     var resultData = {};
     requestData.components.map(item => {
-      resultData[item.id] = runFunc(item.id, 'getData');
+      if (item.form) {
+        resultData[item.id] = runFunc(item.id, 'getData');
+      }
     });
     return resultData;
   };
+
+  const submit = url => {
+    var isOk = check(); // 验证数据是否为空
+
+    if (!isOk) {
+      console.log('提交时 有失败校验。。。');
+      return;
+    }
+    //  获取数据
+    let resultData = getData();
+
+    $.post(url, JSON.stringify(resultData), result => {
+      console.log('提交数据成功', result);
+    });
+
+    // 组件内部提交
+    // requestData.components.map(item => {
+    //   runFunc(item.id, 'submit');
+    // });
+  };
+
   const initInputValue = (componentId, ...rest) => {
     runFunc(componentId, 'setData', ...rest);
   };
+
   const main = (url, data) => {
-    ajaxRequest(url,data, (res) => {
+    requestData = {
+      style: '',
+      components: [
+        {
+          id: 'yhc01',
+          form: true,
+        },
+        {
+          id: 'yhc02',
+          form: false,
+        },
+        {
+          id: 'yhc03',
+          form: true,
+        },
+        {
+          id: 'yhc04',
+          form: true,
+        },
+        {
+          id: 'yhc05',
+          form: true,
+        },
+        {
+          id: 'yhc06',
+          form: false,
+        },
+        {
+          id: 'yhc07',
+          form: true,
+        },
+        {
+          id: 'yhc08',
+          form: false,
+        },
+        {
+          id: 'yhc09',
+          form: false,
+        },
+        {
+          id: 'yhc10',
+          form: false,
+        },
+      ],
+    };
+    configStyle(requestData.style);
+    ajaxRequest(url, data, res => {
       // requestData = res
-      requestData = {
-        style: '#kj_app {background-color: #f00}',
-        components: [
-          {
-            id: 'yhc01',
-          },
-          {
-            id: 'yhc02',
-          },
-          {
-            id: 'yhc03',
-          },
-          {
-            id: 'yhc04',
-          },
-          {
-            id: 'yhc05',
-          },
-          {
-            id: 'yhc06',
-          },
-          {
-            id: 'yhc07',
-          },
-          {
-            id: 'yhc08',
-          },
-          {
-            id: 'yhc09',
-          },
-          {
-            id: 'yhc10',
-          },
-        ],
-      }
-      configStyle(requestData.style);
-    })
+      // configStyle(requestData.style);
+    });
   };
 
   window.TA505 = {
@@ -143,3 +156,4 @@ $(function () {
     dispatchEvent,
   };
 });
+
