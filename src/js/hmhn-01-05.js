30f9d0dc311ea5d779930cf6832c517024b7d893
eiiizong <eiiizong@163.com>
2021-12-21 17:36:27 +0800

    ....

--- src/js/hmhn-01-05.js
+++ src/js/hmhn-01-05.js
@@ -1,25 +1,42 @@
-
-var hnhm0104 = new Vue(
-  {
-    el: '#subsidy_details',
-    data() {
-      return {
-
-      }
-    },
-    created() {
-      console.log('hnhm0101 created');
-    },
-    mounted() {
-      console.log('hnhm0101 mounted');
-      this.initData()
-    },
-    methods: {
-      // 初始化数据
-      initData() {
-        console.log('hnhm0101 initData');
-      }
+var hnhm0104 = new Vue({
+  el: '#subsidy_details',
+  data() {
+    return {
+      tabData: [
+        {
+          id: '1',
+          name: '补贴发放详情',
+          checked: true,
+        },
+        {
+          id: '2',
+          name: '补贴申报详情',
+          checked: false,
+        },
+      ],
     }
-  }
-)
-console.log('hnhm0101',hnhm0101);
+  },
+  created() {
+    console.log('hnhm0101 created')
+  },
+  mounted() {
+    console.log('hnhm0101 mounted')
+    this.initData()
+  },
+  methods: {
+    // 初始化数据
+    initData() {
+      console.log('hnhm0101 initData')
+    },
+    handleClickTab(id) {
+      const tabData = this.tabData
+      tabData.map(item => {
+        item.checked = false
+        if(item.id===id) {
+          item.checked = true
+        }
+      })
+    }
+  },
+})
+console.log('hnhm0101', hnhm0101)
