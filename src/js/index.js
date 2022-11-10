fa61210c2417b60c4c50e34532d5b32860fdeeb6
eiiizong <eiiizong@163.com>
2022-11-10 21:12:59 +0800

    通讯录

--- src/js/index.js
+++ src/js/index.js
@@ -10,12 +10,45 @@
     mounted() {
       this.$nextTick(() => {
         this.initScrollMessageList()
+        this.initScrollMessageConentList()
       })
     },
     methods: {
       initScrollMessageList() {
         this.bsMessageList = BetterScroll.createBScroll(
           this.$refs.ref_message_list,
+          {
+            bounce: false,
+            click: true,
+            scrollY: true,
+            scrollX: false,
+            scrollbar: {
+              fade: true,
+              interactive: true,
+              // 以下配置项 v2.2.0 才支持
+              customElements: [],
+              minSize: 4,
+              scrollbarTrackClickable: true,
+              scrollbarTrackOffsetType: 'step',
+              scrollbarTrackOffsetTime: 300,
+              // 以下配置项 v2.4.0 才支持
+              fadeInTime: 250,
+              fadeOutTime: 500,
+            },
+            mouseWheel: {
+              speed: 10,
+              invert: false,
+              easeTime: 300,
+              discreteTime: 400,
+              throttleTime: 0,
+              dampingFactor: 0.1,
+            },
+          }
+        )
+      },
+      initScrollMessageConentList() {
+        this.bsMessageList = BetterScroll.createBScroll(
+          this.$refs.ref_message_content_list,
           {
             bounce: false,
             click: true,
