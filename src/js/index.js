d4c9b1684b8e415593f34b391aaceeea4c4df4b2
eiiizong <eiiizong@163.com>
2022-11-11 10:44:52 +0800

    123

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,8 @@
       return {
         dialogVisibleMessage: true,
         bsMessageList: null,
+        bsMessageContentList: null,
+        bsMailList: null,
         messageTabActive: 'mailList', // message mailList
         mailListData: [
           {
@@ -214,12 +216,91 @@
           children: 'children',
           label: 'label',
         },
+        tableData: [
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333,
+          },
+
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-04',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1517 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-01',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1519 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-03',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1516 弄',
+            zip: 200333,
+          },
+        ],
       }
     },
     mounted() {
       this.$nextTick(() => {
-        this.initScrollMessageList()
-        this.initScrollMessageConentList()
+        if (this.messageTabActive == 'message') {
+          this.initScrollMessageList()
+          this.initScrollMessageConentList()
+        } else {
+          this.initScrollMailList()
+        }
       })
     },
     methods: {
@@ -256,7 +337,7 @@
         )
       },
       initScrollMessageConentList() {
-        this.bsMessageList = BetterScroll.createBScroll(
+        this.bsMessageContentList = BetterScroll.createBScroll(
           this.$refs.ref_message_content_list,
           {
             bounce: false,
@@ -287,8 +368,41 @@
           }
         )
       },
+      initScrollMailList() {
+        this.bsMailList = BetterScroll.createBScroll(this.$refs.ref_mail_list, {
+          bounce: false,
+          click: true,
+          scrollY: true,
+          scrollX: false,
+          scrollbar: {
+            fade: true,
+            interactive: true,
+            // 以下配置项 v2.2.0 才支持
+            customElements: [],
+            minSize: 4,
+            scrollbarTrackClickable: true,
+            scrollbarTrackOffsetType: 'step',
+            scrollbarTrackOffsetTime: 300,
+            // 以下配置项 v2.4.0 才支持
+            fadeInTime: 250,
+            fadeOutTime: 500,
+          },
+          mouseWheel: {
+            speed: 10,
+            invert: false,
+            easeTime: 300,
+            discreteTime: 400,
+            throttleTime: 0,
+            dampingFactor: 0.1,
+          },
+        })
+      },
       handleClickMailListNode(data) {
+        this.bsMailList.refresh()
         console.log(data)
+      },
+      handleClickTableCell(row) {
+        console.log(row)
       },
     },
   })
