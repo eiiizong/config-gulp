d7aafdb7b462a2fdd33beb7fcb0e76837ecd913e
eiiizong <eiiizong@163.com>
2023-02-27 14:19:01 +0800

    框架搭建

---
+++ src/js/ygdzyktjgpt.js
@@ -0,0 +1,13 @@
+// 框架 请勿改动
+
+;(function () {
+  console.log('四川省以工代赈劳务报酬“一卡通”监管平台')
+  var app = new Vue({
+    el: '#app',
+    data() {
+      return {}
+    },
+    mounted() {},
+    methods: {},
+  })
+})()
