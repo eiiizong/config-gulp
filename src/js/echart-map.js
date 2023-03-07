43ae13f5aac761d0ee81f1eb38cbe3257cd695ef
eiiizong <eiiizong@163.com>
2023-03-07 19:22:12 +0800

    地图

--- src/js/echart-map.js
+++ src/js/echart-map.js
@@ -1,40 +1,68 @@
 // 框架 请勿改动
 
 ;(function () {
-  console.log('四川省以工代赈劳务报酬“一卡通”监管平台')
+  console.log('Echart 地图示例')
   $(function () {
-    console.log('123')
-
     // 地图渲染
     var renderMap = function () {
+      var mapName = '四川省'
       var uploadedDataURL = './lib/json/510000.json'
       // 基于准备好的dom，初始化echarts实例
       var myChart = echarts.init(document.getElementById('echart_map'))
 
-      // 指定图表的配置项和数据
-      var option = {
-        title: {
-          text: 'ECharts 入门示例',
-        },
-        tooltip: {},
-        legend: {
-          data: ['销量'],
-        },
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
-        },
-        yAxis: {},
-        series: [
-          {
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20],
-          },
-        ],
-      }
+      myChart.showLoading()
 
-      // 使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option)
+      $.get(uploadedDataURL, function (geoJson) {
+        myChart.hideLoading()
+
+        echarts.registerMap(mapName, geoJson)
+
+        var option = {
+          backgroundColor: 'transparent',
+          title: {
+            show: false,
+          },
+          series: [
+            {
+              type: 'map',
+              roam: false,
+              mapType: mapName,
+              label: {
+                normal: {
+                  show: true,
+                  textStyle: {
+                    fontSize: 12,
+                    color: '#fff',
+                  },
+                },
+                emphasis: {
+                  textStyle: {
+                    color: '#fff',
+                  },
+                },
+              },
+              itemStyle: {
+                normal: {
+                  borderColor: '#389BB7',
+                  areaColor: '#00b7f5',
+                  borderWidth: 1,
+                  borderColor: '#206ee7',
+                  shadowColor: '#206ee7',
+                  shadowBlur: 10,
+                },
+                emphasis: {
+                  areaColor: '#017ee2',
+                  borderColor: '#017ee2',
+                  borderWidth: 2,
+                },
+              },
+            },
+          ],
+        }
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option)
+      })
     }
 
     window.addEventListener('resize', renderMap, false)
